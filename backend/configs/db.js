78e78f81f760b0cc63cd3d5498af593a38c75053
hydro-justus <fenixkacar3@gmail.com>
2026-08-31 17:32:07 +0300

    Add CloudFormation infrastructure and CI/CD

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-   host: 'localhost',
-   port: '3306',
-   user: 'root',
-   password: '12345678',
-   database: 'react_node_app'
+   host: process.env.DB_HOST,
+   port: process.env.DB_PORT || 3306,
+   user: process.env.DB_USER,
+   password: process.env.DB_PASSWORD,
+   database: process.env.DB_NAME || 'react_node_app'
 });
 
 module.exports = db;
